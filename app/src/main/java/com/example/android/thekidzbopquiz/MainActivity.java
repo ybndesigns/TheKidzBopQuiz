d809276518897cc6d9f5cdef756b1b60f02f5181
Yvonne <ybndesigns@gmail.com>
2018-04-10 01:40:36 -0400

    updated with commentary and a bugfix

--- app/src/main/java/com/example/android/thekidzbopquiz/MainActivity.java
+++ app/src/main/java/com/example/android/thekidzbopquiz/MainActivity.java
@@ -17,13 +17,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //the variable "score" will hold the amount of right answers the user gets
+
+    int score = 0;
+
+    /**
+     * The method that checks all the RadioGroup questions
+     * @param correctAns is the predesignated correct answer that will be checked to see if selected
+     * @param questionGroup is the RadioGroup that the answer belongs to, for the wrongly-selected answer to be
+     *  displayed as incorrect
+     */
 
     public void radioAnswerCheck(RadioButton correctAns, RadioGroup questionGroup) {
         if (correctAns.isChecked()) {
@@ -35,32 +45,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void checkBoxAnswerCheck() {
-        CheckBox lyricsCorrect1 = findViewById(R.id.checkbox_correct1);
-        CheckBox lyricsCorrect2 = findViewById(R.id.checkbox_correct2);
-        CheckBox lyricsFalse1 = findViewById(R.id.checkbox_wrong1);
-        CheckBox lyricsFalse2 = findViewById(R.id.checkbox_wrong2);
-        CheckBox lyricsFalse3 = findViewById(R.id.checkbox_wrong3);
-        TextView questionPrompt = findViewById(R.id.multiple_answers_group);
-        if (lyricsCorrect1.isChecked() && lyricsCorrect2.isChecked()) {
-            CheckBox wrongAns[] = {lyricsFalse1, lyricsFalse2, lyricsFalse3};
-            for (CheckBox lyrics : wrongAns) {
-                if (lyrics.isChecked()) {
-                    questionPrompt.setTextColor(getResources().getColor(R.color.colorWrong));
-                    break;
-                } else {
-                    lyricsCorrect1.setTextColor(getResources().getColor(R.color.colorCorrect));
-                    lyricsCorrect2.setTextColor(getResources().getColor(R.color.colorCorrect));
-                    score += 1;
-                }
-            }
-        } else {
-            questionPrompt.setTextColor(getResources().getColor(R.color.colorWrong));
-        }
-
-    }
-
-
+    /**
+     * The method that checks the bonus EditText question
+     */
     public void textAnswerCheck() {
         EditText youtuberQuestion = (EditText) findViewById(R.id.youtuber_question);
         String youtuberAnswer = youtuberQuestion.getText().toString();
@@ -72,11 +59,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Error message for any required unfulfilled questions
+     */
+
     public void errorMessage() {
         Toast warningMessage = Toast.makeText(this, "Please answer all the questions", Toast.LENGTH_SHORT);
         warningMessage.setGravity(Gravity.CENTER, 0, 0);
         warningMessage.show();
     }
+
+    /**
+     * The onClick method to tally and display the quiz results
+     */
 
     public void submitScore(View v) {
         RadioGroup question1 = (RadioGroup) findViewById(R.id.question_1_radiogroup);
@@ -97,9 +92,14 @@ public class MainActivity extends AppCompatActivity {
         CheckBox lyricsFalse2 = findViewById(R.id.checkbox_wrong2);
         CheckBox lyricsFalse3 = findViewById(R.id.checkbox_wrong3);
 
+        //Making sure the score is set to zero before counting.
         score = 0;
+
+        //Arrays to make grading the questions more efficient
         CheckBox lyricsQuestion[] = {lyricsCorrect1, lyricsCorrect2, lyricsFalse1, lyricsFalse2, lyricsFalse3};
         RadioGroup radiogroups[] = {question1, question2, question3, question4, question5, question6};
+
+        //Checking to see if any of the required questions are unanswered. If so, the error message is displayed.
         for (RadioGroup question : radiogroups) {
             if (question.getCheckedRadioButtonId() == -1) {
                 errorMessage();
@@ -118,20 +118,49 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        //Checking the RadioGroup questions
         radioAnswerCheck(question1Ans, question1);
         radioAnswerCheck(question2Ans, question2);
         radioAnswerCheck(question3Ans, question3);
         radioAnswerCheck(question4Ans, question4);
         radioAnswerCheck(question5Ans, question5);
         radioAnswerCheck(question6Ans, question6);
-        checkBoxAnswerCheck();
+
+        //Checking the multiple-answer question that utilizes CheckBoxes
+        int counter = 1;
+        TextView questionPrompt = findViewById(R.id.multiple_answers_group);
+        if (lyricsCorrect1.isChecked() && lyricsCorrect2.isChecked()) {
+            CheckBox wrongAns[] = {lyricsFalse1, lyricsFalse2, lyricsFalse3};
+            for (CheckBox lyrics : wrongAns) {
+                if (lyrics.isChecked()) {
+                    questionPrompt.setTextColor(getResources().getColor(R.color.colorWrong));
+                    break;
+                } else if (counter < (wrongAns.length)) {
+                    counter += 1;
+                    continue;
+                }
+                else {
+                    lyricsCorrect1.setTextColor(getResources().getColor(R.color.colorCorrect));
+                    lyricsCorrect2.setTextColor(getResources().getColor(R.color.colorCorrect));
+                    score += 1;
+                }
+            }
+        } else {
+            questionPrompt.setTextColor(getResources().getColor(R.color.colorWrong));
+        }
+
+        //Bonus question check
         textAnswerCheck();
+
+        //Final scoring
         TextView finalMessage = findViewById(R.id.answer_text);
         LinearLayout answerReveal = findViewById(R.id.answer_reveal);
         String scoreText = "Your Final score is:\n" + score + " out of 7";
+        //The toast that displays a score
         Toast toast = Toast.makeText(this, "You got " + score + " correct. See more info below", Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
+        //The more detailed score as well as extra options being displayed at the bottom
         EditText youtuberQuestion = (EditText) findViewById(R.id.youtuber_question);
         String youtuberAnswer = youtuberQuestion.getText().toString();
         if (youtuberAnswer.equals("jacksfilms")) {
@@ -142,33 +171,49 @@ public class MainActivity extends AppCompatActivity {
         }
         finalMessage.setText(scoreText);
         answerReveal.setVisibility(View.VISIBLE);
+        //Auto-scrolling to the revealed score for ease of viewing
         ((ScrollView) findViewById(R.id.scrollView)).requestChildFocus(answerReveal, answerReveal);
     }
 
+    /**
+     *onClick method to erase previous stored values
+     */
     public void reset(View v) {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
     }
 
+    /**
+     * Method to reveal a correct answer for the RadioGroup questions as well as their paired factual tidbit
+     * @param textString refers to the correct answer to the question
+     * @param infodump is the extra information that will be displayed
+     */
     public void rightAnswersReveal(TextView textString, TextView infodump) {
         textString.setTextColor(getResources().getColor(R.color.colorCorrect));
         infodump.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * OnClick method to show all the correct answers for the quiz as well as extra information
+     */
     public void viewAnswers(View v) {
+        //For the RadioGroups
         rightAnswersReveal((RadioButton) findViewById(R.id.question_1_correct), (TextView) findViewById(R.id.question_1_infodump));
         rightAnswersReveal((RadioButton) findViewById(R.id.question_2_correct), (TextView) findViewById(R.id.question_2_infodump));
         rightAnswersReveal((RadioButton) findViewById(R.id.question_3_correct), (TextView) findViewById(R.id.question_3_infodump));
         rightAnswersReveal((RadioButton) findViewById(R.id.question_4_correct), (TextView) findViewById(R.id.question_4_infodump));
         rightAnswersReveal((RadioButton) findViewById(R.id.question_5_correct), (TextView) findViewById(R.id.question_5_infodump));
         rightAnswersReveal((RadioButton) findViewById(R.id.question_6_correct), (TextView) findViewById(R.id.question_6_infodump));
+        //For the multiple-answer question using CheckBoxes
         ((CheckBox) findViewById(R.id.checkbox_correct1)).setTextColor(getResources().getColor(R.color.colorCorrect));
         rightAnswersReveal(((CheckBox) findViewById(R.id.checkbox_correct2)), (TextView) findViewById(R.id.question_7_infodump));
+        //For the EditText bonus question
         EditText question8 = findViewById(R.id.youtuber_question);
         question8.setText("jacksfilms");
         question8.getBackground().mutate().setColorFilter(getResources().getColor(R.color.colorCorrect), PorterDuff.Mode.SRC_ATOP);
         findViewById(R.id.question_8_infodump).setVisibility(View.VISIBLE);
+        //Scrolling up to the first question for ease of viewing
         LinearLayout theTop = findViewById(R.id.the_top);
         ((ScrollView) findViewById(R.id.scrollView)).requestChildFocus(theTop, theTop);
     }
